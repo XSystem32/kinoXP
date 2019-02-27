0f44edfce51a15e82fc3c05b1bd78301cba32fa3
XSystem32 <x_system16@hotmail.com>
2019-02-27 10:34:21 +0100

    First commit

---
+++ src/test/java/scrum/kinoxp/KinoxpApplicationTests.java
@@ -0,0 +1,16 @@
+package scrum.kinoxp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KinoxpApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
