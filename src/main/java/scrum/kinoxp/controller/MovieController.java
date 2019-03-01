c19addb586e65dccedd807e673ed01cec35acdeb
VLC <valentinleonchristensen@gmail.com>
2019-03-01 12:00:01 +0100

    added annotations to a controller and servie

--- src/main/java/scrum/kinoxp/controller/MovieController.java
+++ src/main/java/scrum/kinoxp/controller/MovieController.java
@@ -1,4 +1,15 @@
 package scrum.kinoxp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import scrum.kinoxp.service.MovieService;
+
+@Controller
 public class MovieController {
+
+    @Autowired
+    MovieService movieService;
+
+
+
 }
--- src/main/java/scrum/kinoxp/service/MovieService.java
+++ src/main/java/scrum/kinoxp/service/MovieService.java
@@ -1,4 +1,7 @@
 package scrum.kinoxp.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MovieService {
 }
