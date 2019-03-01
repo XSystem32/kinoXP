00f43e7d9a324081d72fe90aa879b183f5f5910c
Jonathan Laage Olsen <jonathan.laage.olsen@gmail.com>
2019-03-01 13:41:56 +0100

    dummy data

--- src/main/java/scrum/kinoxp/repository/movieRepo/MovieRepository.java
+++ src/main/java/scrum/kinoxp/repository/movieRepo/MovieRepository.java
@@ -16,11 +16,12 @@ public class MovieRepository implements MovieRepo {
 
     public void repoInit() {
         Movie LOTR = new Movie(1,"Lotr","asdasdasd","https://www.syfy.com/sites/syfy/files/styles/1200x680/public/wire/legacy/LOTR.png","https://www.youtube.com/watch?v=Pki6jbSbXIY","2:22");
+        Movie Starwars = new Movie(2,"StarWars","Two Jedi Knights escape a hostile blockade to find allies and come across a young boy who may bring balance to the Force, but the long dormant Sith resurface to claim their old glory.","https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY999_SX666_AL_.jpg","https://www.youtube.com/watch?v=uh_YNjydvRI","1:53");
+        Movie backToTheFuture = new Movie(3,"back to the future","Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.","https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,643,1000_AL_.jpg","https://www.imdb.com/title/tt0088763/videoplayer/vi252380953?ref_=tt_ov_vi","1:45" );
         movies.add(LOTR);
-        movies.add(LOTR);
-        movies.add(LOTR);
-        movies.add(LOTR);
-        movies.add(LOTR);
+        movies.add(Starwars);
+        movies.add(backToTheFuture);
+
         Movie movie = new Movie();
 
 
