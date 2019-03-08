23e5be3189ef5e107b5fa30c1d4b4991c605950f
Jonathan Laage Olsen <jonathan.laage.olsen@gmail.com>
2019-03-08 13:54:44 +0100

    show layers

---
+++ src/main/java/scrum/kinoxp/service/ShowService.java
@@ -0,0 +1,42 @@
+package scrum.kinoxp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import scrum.kinoxp.model.Show;
+import scrum.kinoxp.repository.showRepo.ShowRepo;
+import scrum.kinoxp.repository.showRepo.ShowRepository;
+
+import java.util.List;
+
+@Service
+public class ShowService implements ShowRepo {
+
+    @Autowired
+    ShowRepository showRepo;
+
+
+    @Override
+    public boolean createShow(Show show) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteShow(int index) {
+        return false;
+    }
+
+    @Override
+    public boolean updateShow(Show show) {
+        return false;
+    }
+
+    @Override
+    public Show getShow(int index) {
+        return null;
+    }
+
+    @Override
+    public List<Show> getShows() {
+        return null;
+    }
+}
