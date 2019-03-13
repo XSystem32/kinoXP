4e666057b268a9caeb596b28c40c8f3c4d248d5b
VLC <valentinleonchristensen@gmail.com>
2019-03-13 11:24:32 +0100

    removed origin al move from show model

--- src/main/java/scrum/kinoxp/model/Show.java
+++ src/main/java/scrum/kinoxp/model/Show.java
@@ -14,7 +14,7 @@ public class Show {
     }
 
     public Show(int id, Movie movie, List<Booking> bookings, String date_time, Theater theater) {
-        this.movie = movie;
+        this.displayMovie = movie;
         this.id = id;
         this.bookings = bookings;
         this.date_time = date_time;
@@ -35,14 +35,6 @@ public class Show {
 
     public void setBookings(List<Booking> bookings) {
         this.bookings = bookings;
-    }
-
-    public Movie getMovie() {
-        return movie;
-    }
-
-    public void setMovie(Movie movie) {
-        this.movie = movie;
     }
 
     public String getDate_time() {
@@ -73,7 +65,6 @@ public class Show {
     public String toString() {
         return "Show{" +
                 "id=" + id +
-                ", movie=" + movie +
                 ", bookings=" + bookings +
                 ", date_time='" + date_time + '\'' +
                 ", theater=" + theater +
